67a863bfae3259f5e76793c500989986ca22615d
vikas <vikas70607@gmail.com>
2022-05-17 18:22:23 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Time Slab</th>
+            <th>Task</th>
+            <th>Time taken (in minutes)</th>
+            <th>Important?</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="text" /></td>
+            <td>Mark</td>
+            <td><input type="number" name="num1"/></td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td><input type="text" /></td>
+            <td>Jacob</td>
+            <td><input type="number" name="num2"/></td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td><input type="text" /></td>
+            <td >Larry</td>
+            <td><input type="number" name="num3"/></td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <p id="new"></p>
+      <button onclick="add()">Add</button>
+      
+
+      
     </div>
+    
   );
 }
 
